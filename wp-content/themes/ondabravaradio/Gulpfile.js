fcc012838d521282d691eaf8a751a7a535de8066
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-02-07 18:45:07 -0600

    compress js

--- wp-content/themes/ondabravaradio/Gulpfile.js
+++ wp-content/themes/ondabravaradio/Gulpfile.js
@@ -48,7 +48,7 @@ gulp.task('js', function () {
 
     ])
     //.pipe(browserify())
-    //.pipe(uglify({ compress: true }))
+    .pipe(uglify({ compress: true }))
     //.pipe(stripDebug())
     .pipe(concat('bundle.js'))
     .pipe(gulp.dest('./js'))
