ab06471b74df29fcbe36a9a2b362b92e54b2b985
Alonso Avotz <alonso@avotz.com>
2018-04-09 17:24:56 -0600

    fix player mobile

--- wp-content/themes/ondabravaradio/page-home.php
+++ wp-content/themes/ondabravaradio/page-home.php
@@ -9,7 +9,7 @@ get_header(); ?>
       <div class="player-container">
        <?php dynamic_sidebar('sidebar-player'); ?>
 
-        <span class="text-play">Dale play para escuchar <i class="fa fa-play"></i></span>
+       
           <div id="player">
             <audio controls preload="none" autoplay class="player-mobile"><source src="http://167.114.210.232:8221/;" type="audio/aac"></audio>
             <!-- <div id="rtmp-player">
@@ -23,6 +23,7 @@ get_header(); ?>
             </div> -->
           
           </div>
+           <span class="text-play">Dale play para escuchar <i class="fa fa-play"></i></span>
 
       </div>
     </div>
